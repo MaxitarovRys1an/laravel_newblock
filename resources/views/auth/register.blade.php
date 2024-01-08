89820066ce0d674c747f916b54b773094d1d1296
MahitarovRys1an <mahitarov.rus@gmail.com>
2024-01-08 21:04:39 +0300

    first commit

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container my-5">
+        <h1 class="text-center">Registration</h1>
+        <div class="col-sm-6 mx-auto">
+            <form action="/register" method="post">
+                @csrf
+                <div class="mb-3">
+                    <input name="name" type="text" class="form-control" placeholder="UserName">
+                </div>
+                <div class="mb-3">
+                    <input name="email" type="email" class="form-control" placeholder="Email">
+                </div>
+                <div class="mb-3">
+                    <input name="password" type="password" class="form-control" placeholder="Password">
+                </div>
+                <div class="mb-3">
+                    <input name="password_confirmation" type="password" class="form-control" placeholder="Confirmation">
+                </div>
+                <div class="mb-3">
+                    <input  type="submit" class="form-control btn btn-primary" value="Registration">
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+{{--<x-guest-layout>--}}
+{{--    <form method="POST" action="{{ route('register') }}">--}}
+{{--        @csrf--}}
+
+{{--        <!-- Name -->--}}
+{{--        <div>--}}
+{{--            <x-input-label for="name" :value="__('Name')" />--}}
+{{--            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />--}}
+{{--            <x-input-error :messages="$errors->get('name')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Email Address -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="email" :value="__('Email')" />--}}
+{{--            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />--}}
+{{--            <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Password -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="password" :value="__('Password')" />--}}
+
+{{--            <x-text-input id="password" class="block mt-1 w-full"--}}
+{{--                            type="password"--}}
+{{--                            name="password"--}}
+{{--                            required autocomplete="new-password" />--}}
+
+{{--            <x-input-error :messages="$errors->get('password')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Confirm Password -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />--}}
+
+{{--            <x-text-input id="password_confirmation" class="block mt-1 w-full"--}}
+{{--                            type="password"--}}
+{{--                            name="password_confirmation" required autocomplete="new-password" />--}}
+
+{{--            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <div class="flex items-center justify-end mt-4">--}}
+{{--            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">--}}
+{{--                {{ __('Already registered?') }}--}}
+{{--            </a>--}}
+
+{{--            <x-primary-button class="ms-4">--}}
+{{--                {{ __('Register') }}--}}
+{{--            </x-primary-button>--}}
+{{--        </div>--}}
+{{--    </form>--}}
+{{--</x-guest-layout>--}}
